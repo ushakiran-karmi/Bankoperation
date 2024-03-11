eda427b76c33ebb5c7a1f71f7393734dd19614c1
collegemanagement <ushakirankarmankar@gmail.com>
2024-03-11 23:22:45 +0530

    code completed

---
+++ createaccount.py
@@ -0,0 +1,18 @@
+
+
+class Bank:
+    bankname="SBI"
+    branch="SBI,KTL,INDIA"
+    
+    
+    #create account
+    def __init__(self,username,pancard,address):
+        self.username=username
+        self.pancard=pancard
+        self.address=address
+
+username=input("ENTER YOUR USERNAME:")
+pancard=input("ENTER YOUR PANCARD:")
+address=input("ENTER YOUR ADDRESS:")
+
+b=Bank(username,pancard,address)        
