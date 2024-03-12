9efc4849cefe585a49ce63c4080231dcc5ea97f8
collegemanagement <ushakirankarmankar@gmail.com>
2024-03-12 13:06:55 +0530

    complete the code

--- completecode.py
+++ completecode.py
@@ -9,9 +9,29 @@ class Bank:
         self.pancard=pancard
         self.address=address
         self.accountbalance=15000
+        print(f"{self.username} account created successfully in SBI Bank")
         
-username=input("enter the username")
-pancard=input("enter the pancard")
-address=input("enter the address")        
+    def deposit(self,account):
+        self.accountbalance=self.accountbalance+account
+        print(f"{amount} deposite successfully")
         
-b=Bank(username,pancard,address)        
+        
+
+print(f"WELCOME TO THE {Bank.bank} {Bank.branch} ")         
+username=input("enter the username: ")
+pancard=input("enter the pancard: ")
+address=input("enter the address: ")        
+        
+b=Bank(username,pancard,address)  
+
+
+while True():
+    print("PLEASE SELECT ANY ABOVE OPTION")
+    print("1.Deposite\n2.Withdraw\n3.Ministatement\n4.Stop")
+    option=int(print(""))
+    
+    if option =='1':
+        amount=float(print("enter the deposit amount"))
+        b.deposit()
+    if option =='4':
+        print("Thank for using the SBI Bank account for transiction")    
