a8a49bb83c444bf2819c00fa169b28e05f84738a
collegemanagement <ushakirankarmankar@gmail.com>
2024-03-12 13:56:05 +0530

    added deposit code

---
+++ deposite/code.py
@@ -0,0 +1,17 @@
+#Deposit
+def deposit(self,amount):
+    self.balance=self.balance+amount
+    print(f'{amount} deposited successfully')
+    
+while True:
+    print("please select any option:")
+    print("1.Deposit\n2.Withdraw\n3.Ministatement\n4.stop")
+    option=int(input(""))
+    
+    
+    if option=='1':
+        amount=float(input('Enter the Deposit amount'))
+        b.deposit(amount)
+    if option=='4':
+        print("Thank you for using SBI bank for transiction")  
+        break  
